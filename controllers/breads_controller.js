892d2ee0f91ecb4eaac9ae3f8a8dad76e0c44df2
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2022-11-21 21:02:22 -0800

    Created route for index

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,10 +2,19 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
+
 // INDEX
+//localhost:3003/breads
 breads.get('/', (req, res) => {
-  res.send(Bread)
+    res.render('Index',
+      {
+        breads: Bread
+      }
+    )
+  // res.send(Bread)
 })
+
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
