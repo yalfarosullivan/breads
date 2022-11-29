7b74a5fc27fe4eca341787c6fbdfd1bdf493a51f
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2022-11-28 18:58:34 -0800

    added delete route

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -24,12 +24,14 @@ breads.get('/new', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
     })
   } else {
-    res.send('404')
+    res.render('404')
   }
 })
+
 // CREATE
 breads.post('/', (req, res) => {
   if (!req.body.image) {
@@ -45,6 +47,11 @@ breads.post('/', (req, res) => {
 })
 
 
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
 
 
   
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // DEPENDENCIES
 const express = require('express')
+const methodOverride = require('method-override')
+
 
 // CONFIGURATION
 require('dotenv').config()
@@ -12,6 +14,8 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
 
 
 
